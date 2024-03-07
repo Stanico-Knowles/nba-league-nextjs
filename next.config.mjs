f90b497f3ab32b2096755007856e6c28b37422c0
Stanico Knowles <ashadknowles@gmail.com>
2024-03-06 19:22:34 -0500

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "a.espncdn.com",
+        port: "",
+        pathname: "/i/**",
+      },
+    ],
+    formats: ["image/avif", "image/webp"],
+  },
+};
 
 export default nextConfig;
